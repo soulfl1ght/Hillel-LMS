82e259a5c380b770f5f3b3ee6c990965c790643f
soulfl1ght <139794936+soulfl1ght@users.noreply.github.com>
2023-08-30 12:15:22 +0200

    Add files via upload

---
+++ time measure.py
@@ -0,0 +1,17 @@
+import time
+
+def measure_time(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        execution_time = end_time - start_time
+        print(f"Function '{func.__name__}' took {execution_time:.6f} seconds to execute.")
+        return result
+    return wrapper
+
+@measure_time
+def fct():
+    time.sleep(2)
+
+fct()
